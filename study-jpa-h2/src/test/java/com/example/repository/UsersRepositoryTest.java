9949e2dc2524b03d1b30bd841c1d522971203c28
Igor <132262b@naver.com>
2022-12-19 23:39:08 +0900

    test 추가

--- study-jpa-h2/src/test/java/com/example/repository/UsersRepositoryTest.java
+++ study-jpa-h2/src/test/java/com/example/repository/UsersRepositoryTest.java
@@ -1,11 +1,16 @@
 package com.example.repository;
 
 import com.example.domain.Users;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+import java.util.Optional;
+
 
 @SpringBootTest
 class UsersRepositoryTest {
@@ -15,8 +20,62 @@ class UsersRepositoryTest {
 
     @Test
     void crud() {
-        usersRepository.save(new Users());
+        // 정렬
+        // List<Users> users = usersRepository.findAll(Sort.by(Sort.Direction.DESC, "name"));
 
-        usersRepository.findAll().forEach(System.out::println);
+        // Lists.newArrayList() 는 assertj 에서 제공해주는 테스트용 함수.
+        List<Users> users = usersRepository.findAllById(Lists.newArrayList(1L,2L,3L));
+
+        users.forEach(System.out::println);
     }
+
+    @Test
+    void crud_sort() {
+        // 정렬
+        // name 컬럼 기준 역순 조회
+        List<Users> users = usersRepository.findAll(Sort.by(Sort.Direction.DESC, "name"));
+
+        users.forEach(System.out::println);
+    }
+
+    @Test
+    void crud_findAllById_in() {
+        //in 조회
+        // Lists.newArrayList() 는 assertj 에서 제공해주는 테스트용 함수.
+        List<Users> users = usersRepository.findAllById(Lists.newArrayList(1L,2L,3L));
+
+        users.forEach(System.out::println);
+    }
+
+    @Test
+    void crud_saveAll() {
+        Users users1 = Users.builder()
+                .email("이것은 이메일이오1111111")
+                .name("이것은 이름이오11111111")
+                .build();
+
+        Users users2 = Users.builder()
+                .email("이것은 이메일이오22222222")
+                .name("이것은 이름이오2222222")
+                .build();
+
+        usersRepository.saveAll(Lists.newArrayList(users1,users2));
+
+    }
+
+    @Test
+    //@Transactional
+    void crud_getOne() {
+        // org.hibernate.LazyInitializationException: could not initialize proxy 발생
+
+        Users users =  usersRepository.getOne(1L);
+        System.out.println(users);
+    }
+
+    @Test
+    void crud_findById() {
+        Optional<Users> users =  usersRepository.findById(234L);
+        System.out.println(users);
+    }
+
 }
