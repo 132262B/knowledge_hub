214eb325db213d4c93ec04b9b5b3dfde3a0f3f91
Igor <132262b@naver.com>
2023-01-12 23:19:59 +0900

    프로젝트 생성

---
+++ boot-jpa-inflearn/src/main/java/jpabook/jpashop/hello/MemberRepository.java
@@ -0,0 +1,27 @@
+package jpabook.jpashop.hello;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository //extends JpaRepository<Member, Long> {
+{
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+
+
+
+}
