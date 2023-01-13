09a5d3a0862a96e876047dc03f1c20089aa29918
Igor <132262b@naver.com>
2023-01-13 10:52:02 +0900

    프로젝트 생성

--- boot-jpa-if/src/test/java/jpabook/jpashop/hello/MemberRepositoryTest.java
+++ boot-jpa-if/src/test/java/jpabook/jpashop/hello/MemberRepositoryTest.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class MemberRepositoryTest {
