17a4efb236c4bcbf5f464daadf3e5a5ead56aeb3
Igor <132262b@naver.com>
2023-01-14 22:36:33 +0900

    member 로직,테스트코드 개발

---
+++ boot-jpa-if/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,63 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    private MemberService memberService;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private EntityManager em;
+
+    @Test
+    void 회원가입() {
+
+        // given
+        Member member = new Member();
+        member.setName("kim");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        em.flush();
+        assertEquals(member, memberRepository.findOne(saveId));
+    }
+
+    @Test
+    void 중복_회원_예외() {
+        // given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        // when
+        memberService.join(member1);
+
+        // then
+        assertThrows(IllegalStateException.class,() -> {
+            memberService.join(member2);
+        }, "에러가 발생하지 않음");
+
+
+    }
+}
