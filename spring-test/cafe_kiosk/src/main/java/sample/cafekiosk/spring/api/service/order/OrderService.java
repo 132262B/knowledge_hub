1ce83a9340bb4ab238a0804c30e5225bc4316b87
Igor <132262b@naver.com>
2023-10-15 23:57:25 +0900

    Test 공부, Order 중복 테스트 추가 및 통합테스트를 위한 AfterEach로 데이터 삭제 코드 추가

--- spring-test/cafe_kiosk/src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
+++ spring-test/cafe_kiosk/src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
@@ -10,6 +10,8 @@ import sample.cafekiosk.spring.domain.product.ProductRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -23,10 +25,21 @@ public class OrderService {
             LocalDateTime registeredDateTime
     ) {
         List<String> productNumbers = request.getProductNumbers();
-        List<Product> products = productRepository.findAllByProductNumberIn(productNumbers);
 
-        Order order = Order.create(products, registeredDateTime);
+        List<Product> duplicateProducts = findProductsBy(productNumbers);
+
+        Order order = Order.create(duplicateProducts, registeredDateTime);
         Order savedOrder = orderRepository.save(order);
         return OrderResponse.of(savedOrder);
+    }
+
+    private List<Product> findProductsBy(List<String> productNumbers) {
+        List<Product> products = productRepository.findAllByProductNumberIn(productNumbers);
+        Map<String, Product> productMap = products.stream()
+                .collect(Collectors.toMap(Product::getProductNumber, p -> p));
+
+        return productNumbers.stream()
+                .map(productMap::get)
+                .collect(Collectors.toList());
     }
 }
--- spring-test/cafe_kiosk/src/test/java/sample/cafekiosk/spring/domain/order/OrderTest.java
+++ spring-test/cafe_kiosk/src/test/java/sample/cafekiosk/spring/domain/order/OrderTest.java
@@ -3,15 +3,13 @@ package sample.cafekiosk.spring.domain.order;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import sample.cafekiosk.spring.domain.product.Product;
-import sample.cafekiosk.spring.domain.product.ProductType;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.SELLING;
-import static sample.cafekiosk.spring.domain.product.ProductType.*;
+import static sample.cafekiosk.spring.domain.product.ProductType.HANDMADE;
 
 class OrderTest {
 
