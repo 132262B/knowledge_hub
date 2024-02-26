7dda202d730a235ee8520d77a313d906c135ed1e
Igor <132262b@naver.com>
2024-02-26 09:47:31 +0900

    setting rewritePath

--- simple-gateway/a-service/src/main/kotlin/app/aservice/WelcomeController.kt
+++ simple-gateway/a-service/src/main/kotlin/app/aservice/WelcomeController.kt
@@ -9,6 +9,4 @@ class WelcomeController {
     @GetMapping("/welcome")
     fun welcomeMessage() = "환영합니다. A 서비스 입니다."
 
-    @GetMapping("/a/welcome")
-    fun welcomeMessageA() = "환영합니다. A 서비스 입니다."
 }
---
+++ simple-gateway/gateway/src/main/kotlin/app/springgateway/LoggingFilter.kt
@@ -0,0 +1,40 @@
+package app.springgateway
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.slf4j.MDC
+import org.springframework.cloud.gateway.filter.GatewayFilter
+import org.springframework.cloud.gateway.filter.GatewayFilterChain
+import org.springframework.cloud.gateway.filter.OrderedGatewayFilter
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory
+import org.springframework.core.Ordered
+import org.springframework.stereotype.Component
+import org.springframework.web.server.ServerWebExchange
+import reactor.core.publisher.Mono
+import java.util.*
+
+@Component
+class LoggingFilter : AbstractGatewayFilterFactory<LoggingFilter.LoggingConfig>(LoggingConfig::class.java) {
+
+    private val log = KotlinLogging.logger {}
+
+    override fun apply(config: LoggingConfig): GatewayFilter {
+        val filter = GatewayFilter { exchange, chain ->
+            val uuid = UUID.randomUUID().toString()
+            MDC.put("uuid", uuid) // UUID를 MDC에 넣습니다.
+
+            val request = exchange.request
+            val response = exchange.response
+
+            log.info { "Custom PRE filter: request id: ${request.id}, uuid: $uuid" }
+
+            chain.filter(exchange).then(Mono.fromRunnable {
+                log.info { "Custom Post filter: response id: ${response.statusCode}, uuid: $uuid" }
+                MDC.remove("uuid")
+            })
+        }
+
+        return OrderedGatewayFilter(filter, Ordered.HIGHEST_PRECEDENCE)
+    }
+
+    class LoggingConfig
+}
