2bd08e3fc850b45d06868699befba0d4688c3714
Igor <132262b@naver.com>
2024-03-12 23:49:34 +0900

    test compose 분리

--- spring_testcontainer/build.gradle.kts
+++ spring_testcontainer/build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
     testImplementation("org.springframework.boot:spring-boot-testcontainers")
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.testcontainers:mysql")
