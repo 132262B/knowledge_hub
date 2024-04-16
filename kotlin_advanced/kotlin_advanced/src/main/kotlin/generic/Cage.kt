a008dce231e6302b4b42e015c09a334621dcafa2
Igor <132262b@naver.com>
2024-04-16 23:53:09 +0900

    generic, 선언 지점 변성, 사용 지점 변성 example

--- kotlin_advanced/kotlin_advanced/src/main/kotlin/generic/Cage.kt
+++ kotlin_advanced/kotlin_advanced/src/main/kotlin/generic/Cage.kt
@@ -6,6 +6,14 @@ fun main() {
 
     val fishCage = Cage2<Fish>()
     fishCage.moveFrom(goldFishCage)
+
+    val fishCage2 = Cage2<Fish>()
+    val goldFishCage2 = Cage2<GoldFish>()
+    goldFishCage2.put(GoldFish("금붕어"))
+    goldFishCage2.moveTo(fishCage2)
+
+    val fishCage3: Cage3<Fish> = Cage3()
+    val animalCage3 : Cage3<Animal> = fishCage3
 }
 
 class Cage {
@@ -40,5 +48,37 @@ class Cage2<T> {
 
     fun moveFrom(cage : Cage2<out T>) {
         this.animals.addAll(cage.animals)
+    }
+
+    fun moveTo(cage : Cage2<in T>) {
+        cage.animals.addAll(this.animals)
+        this.animals.clear()
+    }
+}
+
+class Cage3<out T> {
+
+        private val animals: MutableList<T> = mutableListOf()
+
+        fun getFirst() : T {
+            return this.animals.first()
+        }
+
+        fun getAll(): List<T> {
+            return this.animals
+        }
+}
+
+
+class Cage4<in T> {
+
+    private val animals: MutableList<T> = mutableListOf()
+
+    fun put(animal : T) {
+        this.animals.add(animal)
+    }
+
+    fun putAll(animal : List<T>) {
+        this.animals.addAll(animal)
     }
 }
