d03e3568191e53528268985c13c1f045fcc2aeb8
Igor <132262b@naver.com>
2024-04-19 00:29:41 +0900

    generic, typealias example

---
+++ kotlin_advanced/kotlin_advanced/src/main/kotlin/generic/TypeAlias.kt
@@ -0,0 +1,13 @@
+package generic
+
+fun main() {
+
+}
+
+class TypeAlias {
+
+    val strange : strangeMap = mapOf()
+
+}
+
+typealias strangeMap = Map<Pair<String, Int>, Triple<String, String, Long>>
