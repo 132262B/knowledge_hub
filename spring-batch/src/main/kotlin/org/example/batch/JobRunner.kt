b5bbfd050dc59c38f11cdff2d09296adbf2bb15a
Igor <132262b@naver.com>
2024-07-06 23:27:43 +0900

    batch study: batch job parameter setting

---
+++ spring-batch/src/main/kotlin/org/example/batch/JobRunner.kt
@@ -0,0 +1,25 @@
+package org.example.batch
+
+import org.springframework.batch.core.Job
+import org.springframework.batch.core.JobParametersBuilder
+import org.springframework.batch.core.launch.JobLauncher
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
+import org.springframework.stereotype.Component
+
+@Component
+class JobRunner(
+    private val simpleJob: Job,
+    private val jobLauncher: JobLauncher
+) : ApplicationRunner {
+
+    override fun run(args: ApplicationArguments) {
+
+        val jobParameters = JobParametersBuilder()
+            .addString("jobParameter", "jobParameter2")
+            .toJobParameters()
+
+        jobLauncher.run(simpleJob, jobParameters)
+    }
+}
