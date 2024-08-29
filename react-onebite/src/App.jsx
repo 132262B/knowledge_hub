56ac69e79224c6ea542922d797f40c7336138863
Igor <132262b@naver.com>
2024-08-29 16:14:55 +0900

    react study: state와 props 연동 study

--- react-onebite/src/App.jsx
+++ react-onebite/src/App.jsx
@@ -1,28 +1,13 @@
 import './App.css';
 import {useState} from "react";
+import Bulb from "./components/Bulb.jsx";
+import Counter from "./components/Counter.jsx";
 
 function App() {
-    const [count, setCount] = useState(0);
-    const [light, setLight] = useState("OFF")
-
-    console.log(count)
     return (
         <>
-            <div>
-                <h1>{light}</h1>
-                <button onClick={()=>{
-                    setLight(light === 'ON' ? "OFF" : "ON")
-                }}>전구 {light === 'ON' ? "끄기" : "켜기"}</button>
-            </div>
-            <div>
-                <h1>{count}</h1>
-                <button onClick={() => {
-                    setCount(count + 1);
-                }}
-                >
-                    +
-                </button>
-            </div>
+            <Bulb></Bulb>
+            <Counter></Counter>
         </>
     )
 }
