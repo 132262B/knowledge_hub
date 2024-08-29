cd5a28623b97700d8441c7c2a18c9b7ae40bec08
Igor <132262b@naver.com>
2024-08-29 13:34:34 +0900

    react study: state study

--- react-onebite/src/App.jsx
+++ react-onebite/src/App.jsx
@@ -1,25 +1,28 @@
 import './App.css';
-import Header from './components/Header';
-import Footer from "./components/Footer";
-import Main from "./components/Main";
-import Button from "./components/Button"
+import {useState} from "react";
 
 function App() {
+    const [count, setCount] = useState(0);
+    const [light, setLight] = useState("OFF")
 
-    const buttonProps = {
-        text: "메일",
-        color: "red"
-    }
-
+    console.log(count)
     return (
         <>
-            <Button {...buttonProps} />
-            <Button text={"카페"} />
-            <Button
-                on
-                text={"블로그"} >
-                <div>자식요소</div>
-            </Button>
+            <div>
+                <h1>{light}</h1>
+                <button onClick={()=>{
+                    setLight(light === 'ON' ? "OFF" : "ON")
+                }}>전구 {light === 'ON' ? "끄기" : "켜기"}</button>
+            </div>
+            <div>
+                <h1>{count}</h1>
+                <button onClick={() => {
+                    setCount(count + 1);
+                }}
+                >
+                    +
+                </button>
+            </div>
         </>
     )
 }
