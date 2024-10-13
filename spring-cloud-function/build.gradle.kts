6fecb9329149dcb16b0f2d3ffb634b27d9c548e3
Igor <132262b@naver.com>
2024-10-13 23:51:56 +0900

    spring cloud function study 2

--- spring-cloud-function/build.gradle.kts
+++ spring-cloud-function/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
     id("org.springframework.boot") version "3.3.4"
     id("io.spring.dependency-management") version "1.1.6"
 }
@@ -26,6 +27,7 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-function-context")
     implementation("org.springframework.cloud:spring-cloud-function-kotlin")
     implementation("org.springframework.cloud:spring-cloud-function-web")
+    implementation("org.springframework.cloud:spring-cloud-function-adapter-aws")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
@@ -45,4 +47,32 @@ kotlin {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Start-Class"] = "com.example.springcloudfunction.SpringCloudFunctionApplication"
+    }
+}
+
+tasks.assemble {
+    dependsOn("shadowJar")
+}
+
+tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
+    archiveClassifier.set("aws")
+    archiveFileName.set("test.jar")
+    dependencies {
+        exclude("org.springframework.cloud:spring-cloud-function-web")
+    }
+    mergeServiceFiles()
+    append("META-INF/spring.handlers")
+    append("META-INF/spring.schemas")
+    append("META-INF/spring.tooling")
+    append("META-INF/spring/org.springframework.boot.autoconfigure.AutoConfiguration.imports")
+    append("META-INF/spring/org.springframework.boot.actuate.autoconfigure.web.ManagementContextConfiguration.imports")
+    transform(com.github.jengelman.gradle.plugins.shadow.transformers.PropertiesFileTransformer::class.java) {
+        paths.add("META-INF/spring.factories")
+        mergeStrategy = "append"
+    }
 }
--- spring-cloud-function/src/main/kotlin/com/example/springcloudfunction/DemoKotlinConfiguration.kt
+++ spring-cloud-function/src/main/kotlin/com/example/springcloudfunction/DemoKotlinConfiguration.kt
@@ -6,18 +6,14 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 class DemoKotlinConfiguration {
 
-    @Configuration
-    class DemoKotlinConfiguration {
-
-        @Bean
-        fun uppercase(): (String) -> String {
-            return { it.toUpperCase() }
-        }
-
-        @Bean
-        fun lowercase(): (String) -> String {
-            return { it.toLowerCase() }
-        }
+    @Bean
+    fun uppercase(): (String) -> String {
+        println("aaa")
+        return { it.toUpperCase() }
     }
 
+    @Bean
+    fun lowercase(): (String) -> String {
+        return { it.toLowerCase() }
+    }
 }
